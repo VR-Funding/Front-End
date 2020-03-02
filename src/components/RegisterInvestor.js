179c7f6b566687a19641b06b885d20d8d85128bf
AhmedBarre10 <barreahmed600@gmail.com>
2020-03-02 15:00:23 -0500

    register form

--- src/components/RegisterInvestor.js
+++ src/components/RegisterInvestor.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
 
-const RegisterInvestor = () => {
-    return(
-    <div>
-        <h1> Register as Investor </h1>
-    </div>
-    )
+const schema = yup.object().shape({
+  email: yup.string().required(),
+  password: yup
+    .string()
+    .required()
+    
+});
+
+const RegisterInvestor = () => {       
+    const { register, handleSubmit, errors } = useForm({
+        validationSchema: schema
+      });
+      const onSubmit = data => {
+        console.log(data);
+      };
+    
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        
+        <input type="email" name="email" placeholder = "  Email" ref={register} />
+      </label>
+      {errors.email && <p>{errors.email.message}</p>}
+        
+      <label>
+        
+        <input type="password" name="password" placeholder = "  Password" ref= {register} />
+      </label>
+      {errors.password && <p>{errors.password.message}</p>}
+      
+      <button className = "InvestorRegister"type="submit"> Register</button>
+    </form>
+  );
+    
 }
 
 export default RegisterInvestor
