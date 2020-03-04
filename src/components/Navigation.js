70e0ed3095537d2706c82f83d2e5ffe2e50850e1
AhmedBarre10 <barreahmed600@gmail.com>
2020-03-04 11:52:01 -0500

    founderspage

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./StartUpCard.css";
+import logo from "./images/logo1.png";
 
 const Navigation = () => {
   return (
     <div className = "Link">
-      <Link to="/Login">Login</Link>
-      <br />
-      <Link to="/Register">Register</Link>
+      <img className = "logo" src = {logo}/>
+     <ul> <Link to="/Login">Login</Link></ul>
+      <ul> <Link to="/Register">Register</Link></ul>
+      <ul> <Link to="#">Home</Link></ul>
+     <ul> <Link to="#">Contact Us</Link></ul>
+     <ul className = "Logoutnav"> <Link to="#">Log Out</Link></ul>
+
     </div>
   );
 };
--- src/components/StartUpList.js
+++ src/components/StartUpList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axiosWithAuth from '../utils/axiosWithAuth'
+import StartUpCard from "./StartUpCard";
+import StartUpPage from "./StartUpCard";
 
 
 const StartUpList = () => {
@@ -7,18 +9,27 @@ const StartUpList = () => {
 
   useEffect(() => {
     axiosWithAuth()
-    .get("startups")
-    .then(res=> console.log(res.data))
+    .get("https://venture-backend.herokuapp.com/api/startups")
+    .then(res=> setStartUps(res.data))
+
   })
   return (
-    <div>
-      <h1>StartUp List</h1>
+    <div> 
+      <div className = "moveup">
+           <h1 className ="trending"> TRENDING STARTUPS </h1>
+           <p>  All companies are backed by </p>
+           <h4 className = "rights">  ACME Co. & have passed liability checks</h4>
+      </div>
+
+
       {startUps.map(startUp => (
-        <div key={startUp.id}>
-        >
-          </div>
+        <StartUpCard key={startUp.id} startUp = {startUp}/>
+        
+        
       ))}
     </div>
+
+    
   );
 };
 
