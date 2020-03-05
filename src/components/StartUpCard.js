8126ada0967a5ab0d3a1d579d57b0e0ccde60e84
Jason Hemann <43769789+JasonRHemann@users.noreply.github.com>
2020-03-05 15:19:57 -0600

    changed state management

--- src/components/StartUpCard.js
+++ src/components/StartUpCard.js
@@ -5,58 +5,58 @@ import styled from "styled-components"
 import "./StartUpCard.css";
 
 
-const ProjectNames = styled.h4`
-position:relative;
-right:10%;
-bottom:10%
+// const ProjectNames = styled.h4`
+// position:relative;
+// right:10%;
+// bottom:10%
 
-`
-const DivStyle = styled.div`
+// `
+// const DivStyle = styled.div`
 
-// background:white;
-margin:30px;
-display:flex;
+// // background:white;
+// margin:30px;
+// display:flex;
 
-width:26vw;
-flex-direction:column;
-color:black;
-position:relative;
-top:20px;
-box-shadow: 12px 14px 3px 0 rgba(0, 0, 0, 0.2);
+// width:26vw;
+// flex-direction:column;
+// color:black;
+// position:relative;
+// top:20px;
+// box-shadow: 12px 14px 3px 0 rgba(0, 0, 0, 0.2);
 
-&&:hover{
-box-shadow: 22px 34px 3px 0 rgba(0, 0, 0, 0.2);
--moz-transform: scale(1.6) rotate(0deg);
--webkit-transform: scale(1.6) rotate(0deg);
--o-transform: scale(1.5) rotate(0deg);
--ms-transform: scale(1.5) rotate(0deg);
-transform: scale(1.1) rotate(0deg);
--webkit-transition: 0.8s;
--moz-transition: 0.8s;
--o-transition: 0.8s;
--ms-transition: 0.8s;
-transition: 0.8s;
- }
-text-align: center;
-`
+// &&:hover{
+// box-shadow: 22px 34px 3px 0 rgba(0, 0, 0, 0.2);
+// -moz-transform: scale(1.6) rotate(0deg);
+// -webkit-transform: scale(1.6) rotate(0deg);
+// -o-transform: scale(1.5) rotate(0deg);
+// -ms-transform: scale(1.5) rotate(0deg);
+// transform: scale(1.1) rotate(0deg);
+// -webkit-transition: 0.8s;
+// -moz-transition: 0.8s;
+// -o-transition: 0.8s;
+// -ms-transition: 0.8s;
+// transition: 0.8s;
+//  }
+// text-align: center;
+// `
 
-const Imgg = styled.img`
+// const Imgg = styled.img`
 
-width:300px
-`
-const Add = styled.p`
-color:gray;
-position:relative;
-bottom:12%
-`
-const Add2 = styled.p`
-position:relative;
-bottom:12%
-`
+// width:300px
+// `
+// const Add = styled.p`
+// color:gray;
+// position:relative;
+// bottom:12%
+// `
+// const Add2 = styled.p`
+// position:relative;
+// bottom:12%
+// `
 const StartUpCard = (props) => {
   
   return (
-    <DivStyle>
+    <div>
       <div>
       <img className="swan" src="../images/logo1.png"/>
  
@@ -64,13 +64,13 @@ const StartUpCard = (props) => {
       </div>
 
 
-    <ProjectNames>{props.startUp.projectName}</ProjectNames>
+    <h4>{props.startUp.projectName}</h4>
     
-      <Add2>{props.startUp.contract} contract, thats {props.startUp.discount}% off</Add2>
- <Add> {props.startUp.country}, {props.startUp.state} {props.startUp.city}</Add>
+      <p>{props.startUp.contract} contract, thats {props.startUp.discount}% off</p>
+ <p> {props.startUp.country}, {props.startUp.state} {props.startUp.city}</p>
      
       
-    </DivStyle>
+    </div>
     
   );
 };
